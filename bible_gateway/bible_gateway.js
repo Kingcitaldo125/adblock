a7667561f8dd7be7f69904e3afdc4393c8f327ae
Paul <paularelt@gmail.com>
2024-06-22 14:16:31 -0400

    Create bible_gateway.js
    block ads on bible gateway

---
+++ bible_gateway/bible_gateway.js
@@ -0,0 +1,30 @@
+const sidebar_ads = document.getElementsByClassName("sidebar-tall-ad");
+const sidebar_sticky_ads = document.getElementsByClassName("sidebar-tall-ad sticky");
+const footer_ads = document.getElementsByClassName("fs-sticky-footer");
+const sponsor_ads = document.getElementsByClassName("sponsor-ad");
+const passage_bottom_ads = document.getElementsByClassName("passage-bottom-ad");
+
+for (let i = 0; i < sidebar_ads.length; ++i)
+{
+    sidebar_ads[i].remove();
+}
+
+for (let i = 0; i < sidebar_sticky_ads.length; ++i)
+{
+    sidebar_sticky_ads[i].remove();
+}
+
+for (let i = 0; i < footer_ads.length; ++i)
+{
+    footer_ads[i].remove();
+}
+
+for (let i = 0; i < sponsor_ads.length; ++i)
+{
+    sponsor_ads[i].remove();
+}
+
+for (let i = 0; i < passage_bottom_ads.length; ++i)
+{
+    passage_bottom_ads[i].remove();
+}
