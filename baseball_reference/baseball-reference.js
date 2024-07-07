672c499784e1cdde0bf6958c9439631bcb28c257
Paul <paularelt@gmail.com>
2024-07-07 10:52:14 -0400

    Create baseball-reference.js

---
+++ baseball_reference/baseball-reference.js
@@ -0,0 +1,12 @@
+const adblock_ads = document.getElementsByClassName("adblock");
+const footer_ads = document.getElementsByClassName("fs-sticky-footer");
+
+for (let i = adblock_ads.length - 1; i >= 0; --i) {
+    adblock_ads[i].remove();
+}
+
+for (let i = 0; i < footer_ads.length; ++i) {
+    footer_ads[i].remove();
+}
+
+console.log("Removed Ads");
