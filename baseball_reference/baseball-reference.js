55c07655457d81f5e4477df5679cff3013dfb287
Paul <paularelt@gmail.com>
2024-08-04 07:52:59 -0400

    Update baseball-reference.js

--- baseball_reference/baseball-reference.js
+++ baseball_reference/baseball-reference.js
@@ -1,5 +1,6 @@
 const adblock_ads = document.getElementsByClassName("adblock");
 const footer_ads = document.getElementsByClassName("fs-sticky-footer");
+const stathead_ads = document.getElementsByClassName("stathead_ad");
 
 for (let i = adblock_ads.length - 1; i >= 0; --i) {
     adblock_ads[i].remove();
@@ -7,6 +8,10 @@ for (let i = adblock_ads.length - 1; i >= 0; --i) {
 
 for (let i = 0; i < footer_ads.length; ++i) {
     footer_ads[i].remove();
+}
+
+for (let i = 0; i < stathead_ads.length; ++i) {
+    stathead_ads[i].remove();
 }
 
 console.log("Removed Ads");
